0dc273d69470b9de1fbb9235fd08e2a19dae163b
Piotr Woloszun <piotr.woloszun@gmail.com>
2016-04-28 07:45:02 +0200

    v 0.1.3

--- src/speech-synthesis/speaker.service.js
+++ src/speech-synthesis/speaker.service.js
@@ -5,6 +5,21 @@ App.factory("speaker", [
     if (!$window.speechSynthesis)
       console.error("Your bowser does not support speechSynthesis()");
 
+    // TODO API
+    // API voices are loaded
+    // $window.speechSynthesis.addEventListener("voiceschanged", function () {
+    //   var voices = $window.speechSynthesis.getVoices();
+    // });
+    // // create speech
+    // var speech = new $window.SpeechSynthesisUtterance("batman!");
+    // speech.voice = voices[0];
+    // // speech end event
+    // speech.addEventListener("end", function (event) {
+    //   console.log(event);
+    // });
+    // // run speech
+    // $window.speechSynthesis.speak(speech);
+
     // TODO
     var speaker = {
       speak: function (text, voice) {
@@ -21,8 +36,3 @@ App.factory("speaker", [
     return speaker;
   }
 ]);
-
-//TODO API
-// $window.speechSynthesis.onvoiceschanged = function () {
-//   var voices = $window.speechSynthesis.getVoices();
-// };
